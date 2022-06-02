1e8cd85167b641d6d43887658ea0fdd054c2fa19
Juli-anna <juliannastarzynska@gmail.com>
2022-06-02 13:49:03 +0200

    Add variables and functions

--- js/functions.js
+++ js/functions.js
@@ -7,3 +7,17 @@ function printMessage(msg){
 function clearMessages(){
     document.getElementById('messages').innerHTML = '';
 }
+
+let computerMove = 'kamień';
+let playerMove = 'papier';
+
+printMessage('Zagrałem ' + computerMove +'! Jeśli Twój ruch to ' + playerMove + ' to wygrywasz!');
+
+let randomFraction = Math.random();
+printMessage('Wylosowany ułamek to: ' + randomFraction);
+
+let calculation = randomFraction * 3 + 1;
+printMessage('Ułamek pomnożony przez 3 i powiększony o 1: ' + calculation);
+
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+printMessage('Liczba po zaokrągleniu w dół to: ' + randomNumber);
